64c7ac504d9bc490fff884c96431032274ce2234
Johnny <jonathanbc2012@gmail.com>
2024-11-14 12:48:18 +0100

    Sección 5 creada con la clase SentenciaIfElse

---
+++ Seccion5-Flujos-De-Control/src/SentenciaIfElse.java
@@ -0,0 +1,23 @@
+
+public class SentenciaIfElse {
+    public static void main(String[] args) {
+
+        float media  = 5.8f;
+
+       if(media >= 6.5) {
+           System.out.println("Felicidades, excelente media");
+       } else if(media >=6.0 ) {
+           System.out.println("Muy buen media");
+       } else if(media >=5.5 ) {
+           System.out.println("Buen media");
+       } else if(media >=5.0 ) {
+           System.out.println("Regular, necesitas esforzarte más");
+       } else if(media >=4.0 ) {
+           System.out.println("Insuficiente");
+       } else {
+           System.out.println("Suspenso");
+       }
+
+        System.out.println("Tu nota media es: " + media);
+    }
+}
