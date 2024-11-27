c4cc204f77c221d40a41ff71e3bd5b2c5d810504
Johnny <jonathanbc2012@gmail.com>
2024-11-27 13:24:33 +0100

    Clase EjemploClaseMath modificada

--- Seccion10-Clase-Math/src/EjemploClaseMath.java
+++ Seccion10-Clase-Math/src/EjemploClaseMath.java
@@ -25,5 +25,34 @@ public class EjemploClaseMath {
         long redondeo3 = Math.round(Math.PI);
         System.out.println("redondeo = " + redondeo3);
 
+        double exp = Math.exp(2);
+        System.out.println("exp = " + exp);
+
+        double log = Math.log(10);
+        System.out.println("log = " + log);
+
+        double potencia = Math.pow(10, 3);
+        System.out.println("potencia = " + potencia);
+
+        double raiz = Math.sqrt(25);
+        System.out.println("raiz = " + raiz);
+
+        double grados = Math.toDegrees(1.57);
+        grados = Math.round(grados);
+        System.out.println("convertir de radianes a grados = " + grados);
+
+        double radianes = Math.toRadians(90.00);
+        System.out.println("convertir de grados a radianes = " + radianes);
+
+        System.out.println("sin(90): " + Math.sin(radianes));
+        System.out.println("cos(90): " + Math.cos(radianes));
+
+        radianes = Math.toRadians(180.00);
+        System.out.println("sin(180): " + Math.sin(radianes));
+        System.out.println("cos(180): " + Math.cos(radianes));
+
+        radianes = Math.toRadians(0.00);
+        System.out.println("sin(0): " + Math.sin(radianes));
+        System.out.println("cos(0): " + Math.cos(radianes));
     }
 }
