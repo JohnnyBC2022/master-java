04b2fb096e8143a09945f11a1ab3473f72879e2c
Johnny <jonathanbc2012@gmail.com>
2024-12-11 12:22:30 +0100

    Clase EjemploMatricesSumar creada

--- Seccion13-Matrices-Arrays-Bidimensionales/src/EjemploMatricesTransponerMatriz.java
+++ Seccion13-Matrices-Arrays-Bidimensionales/src/EjemploMatricesTransponerMatriz.java
@@ -7,7 +7,7 @@ public class EjemploMatricesTransponerMatriz {
 
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
-                a[i][j] = i+j*3;
+                a[i][j] = i + j * 3;
                 System.out.print(a[i][j] + "\t");
             }
             System.out.println();
